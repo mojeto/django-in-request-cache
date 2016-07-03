34ff76df69984d9e0557fb5e4e3018b0d95ae7ea
Jan Nakladal <mojeto1@gmail.com>
2016-07-04 08:52:10 +1200

    prepare test fixture global_request

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -5,7 +5,21 @@
 
 from __future__ import unicode_literals, absolute_import
 
+import django_globals
+import pytest
 from django.test import Client
+from django_globals.middleware import Global
+
+
+@pytest.fixture()
+def global_request():
+    request = {}
+    Global().process_request(request)
+    return request
+
+
+def test_global_request_fixture(global_request):
+    assert global_request is django_globals.globals.request
 
 
 def test_cache_in_request(client):
