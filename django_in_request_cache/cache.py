0dd0165d7c86d1ba13adc4102e19dbb02001389e
Jan Nakladal <mojeto1@gmail.com>
2016-07-04 21:02:43 +1200

    in django request cache added.

--- django_in_request_cache/cache.py
+++ django_in_request_cache/cache.py
@@ -4,3 +4,149 @@
 # Copyright (c) 2016 Jan Nakladal
 
 from __future__ import unicode_literals, absolute_import
+
+import collections
+import time
+import warnings
+
+from django.core.cache.backends.base import BaseCache, DEFAULT_TIMEOUT
+from django_globals import globals as d_global
+
+
+CacheItem = collections.namedtuple('CacheItem', ['value', 'expire_at'])
+
+
+class InRequestCache(BaseCache):
+    cache_name = '_dinr_cache'
+    max_timeout = None
+
+    def __init__(self, params):
+        super(InRequestCache, self).__init__(params)
+        self.cache_name = params.get('cache_name', params.get('CACHE_NAME')) \
+            or self.cache_name
+        self.max_timeout = int(
+            params.get('max_timeout', params.get('MAX_TIMEOUT')) or
+            self.default_timeout
+        )
+
+    @property
+    def request(self):
+        """
+        get current django request object
+        :return:
+        """
+        try:
+            return d_global.request
+        except AttributeError:
+            warnings.warn(
+                'Missing django request object, check you have django_globals.'
+                'middleware.Global in middleware_classes and don\'t use this '
+                'cache out of django http request scope',
+                stacklevel=4
+            )
+
+    @property
+    def cache(self):
+        """
+        get cache object stored in django request
+        :return: cache (dict) instance
+        """
+        request = self.request
+        cache = getattr(request, self.cache_name, None)
+        if not isinstance(cache, collections.MutableMapping):
+            cache = self._initialize_cache(request)
+
+        return cache
+
+    def _initialize_cache(self, request):
+        """
+        Initialize cache in request object
+        :return: cache (dict) instance
+        """
+        cache = {}
+        if request:
+            setattr(request, self.cache_name, cache)
+
+        return cache
+
+    def get_backend_timeout(self, timeout=DEFAULT_TIMEOUT):
+        """
+        Returns the timeout value usable by this backend based upon the provided
+        timeout.
+        """
+        if timeout == DEFAULT_TIMEOUT:
+            timeout = self.default_timeout
+        elif timeout <= 0:
+            # ticket 21147 - avoid time.time() related precision issues
+            timeout = self.max_timeout
+        timeout = min(timeout, self.max_timeout)
+        return time.time() + timeout
+
+    def cache_key(self, key, version=None):
+        """
+        Constructs and validate the key used by all other methods.
+        """
+        key = self.make_key(key, version=version)
+        self.validate_key(key)
+        return key
+
+    def set(self, key, value, timeout=DEFAULT_TIMEOUT, version=None):
+        """
+        Set a value in the cache. If timeout is given, that timeout will be
+        used for the key; otherwise the default cache timeout will be used.
+        """
+        key = self.cache_key(key, version=version)
+        expiration_time = self.get_backend_timeout(timeout)
+        self.cache[key] = CacheItem(value, expiration_time)
+
+    def get(self, key, default=None, version=None):
+        """
+        Fetch a given key from the cache. If the key does not exist, return
+        default, which itself defaults to None.
+        """
+        key = self.cache_key(key, version=version)
+        item = self.cache.get(key)
+        if isinstance(item, CacheItem) and time.time() < item.expire_at:
+            return item.value
+
+        return default
+
+    def delete(self, key, version=None):
+        """
+        Delete a key from the cache, failing silently.
+        """
+        key = self.cache_key(key, version=version)
+        try:
+            del self.cache[key]
+        except KeyError:
+            pass  # delete method fails silently.
+
+    def clear(self):
+        """
+        Remove *all* values from the cache at once.
+        """
+        self._initialize_cache(self.request)
+
+    def add(self, key, value, timeout=DEFAULT_TIMEOUT, version=None):
+        """
+        Set a value in the cache if the key does not already exist. If
+        timeout is given, that timeout will be used for the key; otherwise
+        the default cache timeout will be used.
+
+        Returns True if the value was stored, False otherwise.
+        """
+        if not self.has_key(key, version):
+            self.set(key, value, timeout, version)
+            return True
+
+        return False
+
+    def has_key(self, key, version=None):
+        """
+        Returns True if the key is in the cache and has not expired.
+        """
+        cache_key = self.cache_key(key, version=version)
+        if cache_key not in self.cache:
+            return False
+
+        return super(InRequestCache, self).has_key(key, version)
--- tests/test_cache.py
+++ tests/test_cache.py
@@ -5,15 +5,26 @@
 
 from __future__ import unicode_literals, absolute_import
 
+import time
+import warnings
+
 import django_globals
 import pytest
 from django.test import Client
-from django_globals.middleware import Global
+from django_globals.middleware import Global, globals as d_globals
+
+from django_in_request_cache.cache import InRequestCache, CacheItem
+
+
+class MockRequest(object):
+    """
+    Mock django request object for testing purpose
+    """
 
 
 @pytest.fixture()
 def global_request():
-    request = {}
+    request = MockRequest()
     Global().process_request(request)
     return request
 
@@ -24,3 +35,105 @@ def test_global_request_fixture(global_request):
 
 def test_cache_in_request(client):
     assert isinstance(client, Client)
+
+
+def test_cache_set(global_request):
+    cache = InRequestCache(params={})
+    cache.set('key', 'value')
+    item = getattr(global_request, cache.cache_name)[cache.cache_key('key')]
+    assert isinstance(item, CacheItem)
+    assert item.value == 'value'
+
+
+def test_cache_set_timeout(global_request):
+    cache = InRequestCache(params={})
+    timeout = 2
+    start = time.time()
+    cache.set('key', 'value', timeout=timeout)
+    end = time.time()
+    item = getattr(global_request, cache.cache_name)[cache.cache_key('key')]
+    assert isinstance(item, CacheItem)
+    assert item.expire_at >= start + timeout
+    assert item.expire_at <= end + timeout
+
+
+def test_cache_get(global_request):
+    cache = InRequestCache(params={})
+    expire_at = time.time()
+    cache_data = {
+        cache.cache_key('key'): CacheItem('value', expire_at),
+        cache.cache_key('key2'): CacheItem('value2', expire_at + 2),
+    }
+    setattr(global_request, cache.cache_name, cache_data)
+
+    assert cache.get('key', default='default') == 'default'
+    assert cache.get('key2') == 'value2'
+
+
+def test_cache_add(global_request):
+    cache = InRequestCache(params={})
+    cache_data = {
+        cache.cache_key('key'): CacheItem('value', time.time() + 10)
+    }
+    setattr(global_request, cache.cache_name, cache_data)
+
+    assert cache.add('key', 'value2') == False
+    assert cache.get('key') == 'value'
+
+    assert cache.add('key2', 'value2') == True
+    assert cache.get('key2') == 'value2'
+
+
+def test_missing_request_warning(global_request):
+    cache = InRequestCache(params={})
+    delattr(d_globals, 'request')
+    with warnings.catch_warnings(record=True) as w:
+        # Cause all warnings to always be triggered.
+        warnings.simplefilter("always")
+        # Trigger a warning.
+        assert cache.get('key', default='default') == 'default'
+        # Verify warning
+        assert len(w) == 1
+        assert issubclass(w[-1].category, UserWarning)
+        assert "Missing django request object" in str(w[-1].message)
+
+
+def test_cache_clear(global_request):
+    cache = InRequestCache(params={})
+    in_request_cache = cache.cache
+    assert in_request_cache is getattr(global_request, cache.cache_name)
+    cache.clear()
+    assert in_request_cache is not getattr(global_request, cache.cache_name)
+    assert in_request_cache is not cache.cache
+    assert cache.cache is getattr(global_request, cache.cache_name)
+
+
+def test_cache_delete(global_request):
+    cache = InRequestCache(params={})
+    cache_key = cache.cache_key('key')
+    cache_data = {
+        cache_key: CacheItem('value', time.time() + 10)
+    }
+    setattr(global_request, cache.cache_name, cache_data)
+    assert cache_key in cache_data
+    cache.delete('key')
+    assert cache_key not in cache_data
+
+
+def test_cache_get_backend_timeout(global_request):
+    max_timeout = 2
+    default_timeout = 10
+    cache = InRequestCache(params={
+        'MAX_TIMEOUT': max_timeout, 'TIMEOUT': default_timeout,
+    })
+    start = time.time()
+    timeout = cache.get_backend_timeout()
+    end = time.time()
+    assert timeout >= start + max_timeout
+    assert timeout <= end + max_timeout
+
+    start = time.time()
+    timeout = cache.get_backend_timeout(timeout=1)
+    end = time.time()
+    assert timeout >= start + 1
+    assert timeout <= end + 1
