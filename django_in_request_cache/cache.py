527883858a0b75fed5513bd167379c452039a10c
Jan Nakladal <mojeto1@gmail.com>
2016-07-15 00:01:12 +1200

    cache to cache a cache

--- django_in_request_cache/cache.py
+++ django_in_request_cache/cache.py
@@ -9,6 +9,7 @@ import collections
 import time
 import warnings
 
+from django.core.cache import caches
 from django.core.cache.backends.base import BaseCache, DEFAULT_TIMEOUT
 from django_globals import globals as d_global
 
@@ -149,3 +150,112 @@ class InRequestCache(BaseCache):
             return False
 
         return super(InRequestCache, self).has_key(key, version)
+
+
+class CacheACache(BaseCache):
+    """
+    use fast (memory) cache to cache a "slower" cache
+    """
+
+    def __init__(self, location, params):
+        self.location = location  # isn't used right now
+        self.fast_cache_alias = params.get('fast_cache', params.get(
+            'FAST_CACHE'
+        ))
+        self.fast_cache_timeout = int(
+            params.get('fast_cache_timeout', params.get('FAST_CACHE_TIMEOUT'))
+        ) or 30
+        self.cache_alias = params.get('cache_to_cache', params.get(
+            'CACHE_TO_CACHE'
+        ))
+        super(CacheACache, self).__init__(params)
+        if self.fast_cache_timeout > 30:
+            warnings.warn(
+                'fast_cache_timeout should be low, because we don\'t know for '
+                'how much longer the cached value should be stored.',
+                stacklevel=2
+            )
+
+    @property
+    def fast_cache(self):
+        """
+        It get faster cache backend
+        :return: BaseCache
+        """
+        return caches[self.fast_cache_alias]
+
+    @property
+    def cache(self):
+        """
+        It get slower cache backend
+        :return: BaseCache
+        """
+        return caches[self.cache_alias]
+
+    def fast_cache_set(self, key, value, timeout=DEFAULT_TIMEOUT, version=None):
+        """
+        Set a value in the cache. If timeout is given, that timeout will be
+        used for the key; otherwise the default cache timeout will be used.
+        """
+        if timeout == DEFAULT_TIMEOUT:
+            timeout = self.fast_cache_timeout
+
+        else:
+            timeout = min(self.fast_cache_timeout, timeout)
+
+        self.fast_cache.set(key, value, timeout, version)
+
+    def set(self, key, value, timeout=DEFAULT_TIMEOUT, version=None):
+        """
+        Set a value in the cache. If timeout is given, that timeout will be
+        used for the key; otherwise the default cache timeout will be used.
+        """
+        if timeout == DEFAULT_TIMEOUT:
+            timeout = self.default_timeout
+
+        self.cache.set(key, value, timeout, version)
+        self.fast_cache_set(key, value, timeout, version)
+
+    def get(self, key, default=None, version=None):
+        """
+        Fetch a given key from the cache. If the key does not exist, return
+        default, which itself defaults to None.
+        """
+        value = self.fast_cache.get(key, default=None, version=version)
+        if value is not None:
+            return value
+
+        value = self.cache.get(key, default=None, version=version)
+        if value is not None:
+            self.fast_cache_set(key, value, version=version)
+            return value
+
+        return default
+
+    def delete(self, key, version=None):
+        """
+        Delete a key from the cache, failing silently.
+        """
+        self.fast_cache.delete(key, version=version)
+        self.cache.delete(key, version=version)
+
+    def clear(self):
+        """
+        Remove *all* values from the cache at once.
+        """
+        self.fast_cache.clear()
+        self.cache.clear()
+
+    def add(self, key, value, timeout=DEFAULT_TIMEOUT, version=None):
+        """
+        Set a value in the cache if the key does not already exist. If
+        timeout is given, that timeout will be used for the key; otherwise
+        the default cache timeout will be used.
+
+        Returns True if the value was stored, False otherwise.
+        """
+        if self.cache.add(key, value, timeout, version):
+            self.fast_cache_set(key, value, timeout, version)
+            return True
+
+        return False
